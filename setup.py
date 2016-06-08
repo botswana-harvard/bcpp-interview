d55cc801085a85f38f54f8bb3c8d26e6c09b88bd
erikvw <ew2789@gmail.com>
2016-06-08 17:17:42 +0200

    bump to 0.2dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bcpp-interview',
-    version='0.1',
+    version='0.2dev0',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
