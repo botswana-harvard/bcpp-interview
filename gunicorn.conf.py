8a88c964b18ced76d9352edab7739da7edc29c9b
erikvw <ew2789@gmail.com>
2016-07-08 11:19:24 +0200

    update gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,12 @@
 # gunicorn -c gunicorn.conf.py bcpp_interview.wsgi --pid /Users/erikvw/source/bcpp-interview/logs/gunicorn.pid --daemon
 #
 
-bind = "127.0.0.1:9000"  # Don't use port 80 becaue nginx occupied it already. 
-errorlog = '/Users/erikvw/source/bcpp-interview/logs/gunicorn-error.log'  # Make sure you have the log folder create
-accesslog = '/Users/erikvw/source/bcpp-interview/logs/gunicorn-access.log'
+import os
+
+SOURCE_FOLDER = os.path.expanduser('~/source')
+
+bind = "127.0.0.1:9000"  # Don't use port 80 becaue nginx occupied it already.
+errorlog = os.path.join(SOURCE_ROOT, 'bcpp-interview/logs/gunicorn-error.log')  # Make sure you have the log folder create
+accesslog = os.path.join(SOURCE_ROOT, 'bcpp-interview/logs/gunicorn-access.log')
 loglevel = 'debug'
 workers = 1  # the number of recommended workers is '2 * number of CPUs + 1'
